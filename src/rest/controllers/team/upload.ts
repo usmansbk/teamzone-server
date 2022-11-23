a5587467a3aba204da4df8effab2089d55e1016c
usmansbk <usmansbk@gmail.com>
2022-11-23 22:33:44 +0100

    upload local file

--- src/rest/controllers/team/upload.ts
+++ src/rest/controllers/team/upload.ts
@@ -1,1 +1,27 @@
-export default function uploadLogo() {}
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+
+const upload = multer({
+  dest: "/upload",
+  limits: {
+    fileSize: 2000000,
+  },
+}).single("logo");
+
+export default function uploadLogo(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  upload(req, res, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      const { file } = req;
+      res.json({
+        message: "File uploaded",
+        file,
+      });
+    }
+  });
+}
--- src/rest/controllers/user/upload.ts
+++ src/rest/controllers/user/upload.ts
@@ -1,1 +1,27 @@
-export default function uploadPicture() {}
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+
+const upload = multer({
+  dest: "/upload",
+  limits: {
+    fileSize: 2000000,
+  },
+}).single("avatar");
+
+export default function uploadPicture(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  upload(req, res, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      const { file } = req;
+      res.json({
+        message: "File uploaded",
+        file,
+      });
+    }
+  });
+}
