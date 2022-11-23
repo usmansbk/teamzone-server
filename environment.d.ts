cefa85e4aa40ff846b22bf8c5819072b480f2409
usmansbk <usmansbk@gmail.com>
2022-11-23 23:12:31 +0100

    add s3 bucket

--- environment.d.ts
+++ environment.d.ts
@@ -18,6 +18,7 @@ declare global {
       AWS_ACCESS_KEY_ID: string;
       AWS_SECRET_ACCESS_KEY: string;
       AWS_REGION: string;
+      AWS_S3_BUCKET: string;
     }
   }
 }
--- src/rest/utils/uploader.ts
+++ src/rest/utils/uploader.ts
@@ -1,6 +1,9 @@
 import { GraphQLError } from "graphql";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { nanoid } from "nanoid";
 import { UNSUPPORTED_FILE_TYPE_ERROR } from "src/constants/errors";
+import { s3Client } from "src/services/s3";
 
 const supportedMimeTypes = ["image/jpeg", "image/png"];
 
@@ -23,4 +26,15 @@ export default multer({
       cb(null, true);
     }
   },
+  storage: multerS3({
+    s3: s3Client,
+    bucket: process.env.AWS_S3_BUCKET,
+    metadata(req, file, cb) {
+      const { mimetype, size, fieldname, filename } = file;
+      cb(null, { mimetype, size, fieldname, filename });
+    },
+    key(req, file, cb) {
+      cb(null, `${file.fieldname}/${nanoid()}`);
+    },
+  }),
 });
