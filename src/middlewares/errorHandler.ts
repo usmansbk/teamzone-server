549a97d099cb74443d5feba54c79d3c71608d006
usmansbk <usmansbk@gmail.com>
2022-11-27 11:43:01 +0100

    nit

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler } from "express";
-import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
-import { GraphQLError } from "graphql";
 
 const errorHandlerMiddleware: ErrorRequestHandler = (
   error,
@@ -9,23 +7,8 @@ const errorHandlerMiddleware: ErrorRequestHandler = (
   res,
   _next
 ) => {
-  let err = error;
-  if (
-    error instanceof JsonWebTokenError ||
-    error instanceof TokenExpiredError
-  ) {
-    err = new GraphQLError(error.message, {
-      extensions: {
-        code: error.name,
-        http: {
-          status: 400,
-        },
-      },
-    });
-  }
-
-  res.json({
-    error: err,
+  res.status(500).json({
+    message: (error as Error).message,
   });
 };
 
