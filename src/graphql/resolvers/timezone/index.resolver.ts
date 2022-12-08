9d3e62b3b4832f2d442363f5460d915b7bb0a9ee
usmansbk <usmansbk@gmail.com>
2022-12-08 14:38:19 +0100

    add country flag field

--- src/graphql/resolvers/timezone/index.resolver.ts
+++ src/graphql/resolvers/timezone/index.resolver.ts
@@ -1,9 +1,13 @@
-import { getTimeZones } from "@vvo/tzdb";
+import { getTimeZones, TimeZone } from "@vvo/tzdb";
 
 export default {
   Query: {
     timezones() {
       return getTimeZones({ includeUtc: true });
     },
+  },
+  TimezoneData: {
+    flag: (tz: TimeZone, { height }: { height: number }) =>
+      `https://flagcdn.com/h${height}/${tz.countryCode.toLocaleLowerCase()}.png`,
   },
 };
