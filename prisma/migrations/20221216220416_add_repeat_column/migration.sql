f37a5ba5a519835f3a9d9ac74140c1139dd47944
usmansbk <usmansbk@gmail.com>
2022-12-27 13:40:25 +0100

    add timer

--- prisma/migrations/20221216220416_add_repeat_column/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Meeting" ADD COLUMN     "repeat" JSONB;
---
+++ prisma/migrations/20221227123956_init/migration.sql
@@ -0,0 +1,192 @@
+-- CreateEnum
+CREATE TYPE "TeamRole" AS ENUM ('ADMIN', 'TEAMMATE');
+
+-- CreateEnum
+CREATE TYPE "TimerDirection" AS ENUM ('COUNTUP', 'COUNTDOWN');
+
+-- CreateEnum
+CREATE TYPE "TimerType" AS ENUM ('DATE', 'DURATION');
+
+-- CreateEnum
+CREATE TYPE "TimerState" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "countryCode" TEXT,
+    "timezone" TEXT,
+    "locale" TEXT,
+    "pictureUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TeamMember" (
+    "id" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "memberId" TEXT NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "TeamRole" NOT NULL DEFAULT 'TEAMMATE',
+
+    CONSTRAINT "TeamMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PinnedTeam" (
+    "id" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "memberId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PinnedTeam_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL,
+    "inviteCode" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isArchived" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "bucket" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "mimetype" TEXT NOT NULL,
+    "userAvatarId" TEXT,
+    "teamLogoId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Meeting" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "from" TIMESTAMP(3) NOT NULL,
+    "to" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "timezone" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "repeat" JSONB,
+
+    CONSTRAINT "Meeting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Timer" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "timezone" TEXT,
+    "description" TEXT,
+    "direction" "TimerDirection" NOT NULL,
+    "type" "TimerType" NOT NULL,
+    "startAt" TIMESTAMP(3),
+    "dateTime" TIMESTAMP(3),
+    "durationInMinutes" INTEGER,
+    "state" "TimerState" DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "repeat" JSONB,
+
+    CONSTRAINT "Timer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_TeamToTimer" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_MeetingToTeam" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TeamMember_teamId_memberId_key" ON "TeamMember"("teamId", "memberId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PinnedTeam_teamId_memberId_key" ON "PinnedTeam"("teamId", "memberId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_userAvatarId_key" ON "File"("userAvatarId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_teamLogoId_key" ON "File"("teamLogoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TeamToTimer_AB_unique" ON "_TeamToTimer"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TeamToTimer_B_index" ON "_TeamToTimer"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_MeetingToTeam_AB_unique" ON "_MeetingToTeam"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_MeetingToTeam_B_index" ON "_MeetingToTeam"("B");
+
+-- AddForeignKey
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PinnedTeam" ADD CONSTRAINT "PinnedTeam_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PinnedTeam" ADD CONSTRAINT "PinnedTeam_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userAvatarId_fkey" FOREIGN KEY ("userAvatarId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_teamLogoId_fkey" FOREIGN KEY ("teamLogoId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Meeting" ADD CONSTRAINT "Meeting_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Timer" ADD CONSTRAINT "Timer_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TeamToTimer" ADD CONSTRAINT "_TeamToTimer_A_fkey" FOREIGN KEY ("A") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TeamToTimer" ADD CONSTRAINT "_TeamToTimer_B_fkey" FOREIGN KEY ("B") REFERENCES "Timer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MeetingToTeam" ADD CONSTRAINT "_MeetingToTeam_A_fkey" FOREIGN KEY ("A") REFERENCES "Meeting"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MeetingToTeam" ADD CONSTRAINT "_MeetingToTeam_B_fkey" FOREIGN KEY ("B") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
